0d1afd4bb3b6e53d3b01836794ebb006a69fdb58
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-11-16 20:22:12 -0300

    feat: change EventPattern with MessagePattern so main backend has responses from microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
-import { EventPattern } from '@nestjs/microservices';
+import { MessagePattern } from '@nestjs/microservices';
 import { ItemDTO } from './item.dto';
 import { CreateItemUseCase } from './usecases/create-item.usecase';
 
@@ -11,13 +11,13 @@ export class AppController {
     private readonly createItemUseCase: CreateItemUseCase,
   ) {}
 
-  @EventPattern('get_all_items')
+  @MessagePattern('get_all_items')
   getHello(): string {
     console.log('get hello!');
     return this.appService.getHello();
   }
 
-  @EventPattern('create_item')
+  @MessagePattern('create_item')
   async createItem(@Body() item: ItemDTO) {
     const itemCreated = await this.createItemUseCase.execute(item);
     return {
