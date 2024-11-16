0172b3e723125411f7cda910a9184018acf84e4a
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-11-16 17:39:00 -0300

    feat: migrate createItem method from monolith

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
-import { EventPattern, MessagePattern } from '@nestjs/microservices';
+import { EventPattern } from '@nestjs/microservices';
+import { ItemDTO } from './item.dto';
+import { CreateItemUseCase } from './usecases/create-item.usecase';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly createItemUseCase: CreateItemUseCase,
+  ) {}
 
   @EventPattern('get_all_items')
   getHello(): string {
     console.log('get hello!');
     return this.appService.getHello();
+  }
+
+  @EventPattern('create_item')
+  async createItem(@Body() item: ItemDTO) {
+    const itemCreated = await this.createItemUseCase.execute(item);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Item created successfully',
+      data: itemCreated,
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateItemUseCase } from './usecases/create-item.usecase';
+import { ItemRepository } from './item.repository';
+import { ItemPrismaRepository } from './item.prisma.repository';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: ItemRepository,
+      useClass: ItemPrismaRepository
+    },
+    CreateItemUseCase
+  ],
 })
 export class AppModule {}
